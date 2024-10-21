2fbba50e9b51fc216aa2a0a0fb889f8b59bdb975
sabihajyoti22 <sabihajyoti@gmail.com>
2024-10-21 15:28:53 +0600

    feat: add 404 page

---
+++ src/views/404.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="h-full flex flex-col gap-3 justify-center items-center font-bold">
+    <div class="text-white text-6xl">
+      404
+    </div>
+    <div class="text-secondary text-5xl">
+      Page Not Found
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
---
+++ src/views/Activate.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="h-full flex flex-col gap-3 justify-center items-center font-bold text-white text-3xl">
+    Your account has activated. Now you can <router-link
+      :to="{name: 'home'}"
+      class="text-secondary"
+    >
+      Sign In
+    </router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
